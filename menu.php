3d00af38bb64b40821ac724dc039af060f6200ad
axlopez8 <axlopez0038@gmail.comm>
2021-04-08 23:50:40 -0600

    estilos imagenes y menu principal

---
+++ menu.php
@@ -0,0 +1,94 @@
+<html lang="en">
+<head>
+	<title>Login Tersa</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta charset="UTF-8">
+
+<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+<!--===============================================================================================-->
+</head>
+<style>
+	body{
+		background-image: url(https://image.freepik.com/vector-gratis/fondo-acuarela-cielo-color-pastel_105555-312.jpg)!important;
+		background-size: cover!important;
+		background-attachment: fixed!important;
+		background-repeat: no-repeat!important;
+	}
+</style>
+<body>
+    
+	<div class="limiter">
+		<div class="container-login100">
+			
+
+			<div class="wrap-login100">
+							
+                            <center><h1>Menu Tersa</h1><img src="images/Tersa.jpg" width="150" height="150" HSPACE="10" VSPACE="10" ></center>
+							
+                            <br>
+                            <br>
+				<div class="login100-pic js-tilt border border-3" data-tilt>
+					<img href="productos/productos.php" src="images/producto.png" width="150" height="150" HSPACE="10" VSPACE="10" >
+                    <center> <a href="vistas/productos.php"><button class="btn blue">Registro de Producto</button></a> </center>
+				</div>
+
+
+                <div class="login100-pic js-tilt border border-3" data-tilt>
+					<img href="proveedores/proveedores.php" src="images/proveedores.jpg" width="150" height="150" HSPACE="10" VSPACE="10">
+                    <center> <a href="vistas/proveedores.php"><button class="btn blue">Registro de Proveedores</button></a> </center>
+				</div>
+
+
+                <div class="login100-pic js-tilt border border-3" data-tilt>
+					<img href="clientes/clientes.php" src="images/clientes.png" width="150" height="150" HSPACE="10" VSPACE="10">
+                    <center> <a href="vistas/clientes.php"><button class="btn blue">Registro de Clientes</button></a> </center>
+				</div>
+				<div class="login100-pic js-tilt border border-3" data-tilt>
+					<img href="compras/compras.php" src="images/compras.png" width="150" height="150" HSPACE="10" VSPACE="10" >
+                    <center> <a href="vistas/compras.php"><button class="btn blue">Registro de Compras</button></a> </center>
+				</div>
+
+
+                <div class="login100-pic js-tilt border border-3" data-tilt>
+					<img href="personas/personas.php" src="images/personas.png" width="150" height="150" HSPACE="10" VSPACE="10">
+                    <center> <a href="vistas/personas.php"><button class="btn blue">Registro de Personas</button></a> </center>
+				</div>
+
+
+                <div class="login100-pic js-tilt border border-3" data-tilt>
+					<img href="ventas/ventas.php" src="images/ventas.png" width="150" height="150" HSPACE="10" VSPACE="10">
+                    <center> <a href="vistas/ventas.php"><button class="btn blue">Registro de Ventas</button></a> </center>
+				</div>
+		</div>
+
+	</div>
+
+
+
+
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/tilt/tilt.jquery.min.js"></script>
+	<script >
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+</html>
